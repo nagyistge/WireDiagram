bac4f9928485333fa3223339db0b858a9ab8379c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2014-12-07 20:34:07 +0100

    Refactored custom line renderer

--- WireDiagram.Demo/CustomElements/CustomElementLine.cs
+++ WireDiagram.Demo/CustomElements/CustomElementLine.cs
@@ -169,173 +169,139 @@ namespace WireDiagram.CustomElements
 
         #endregion
 
-        #region Contructor
+        #region Fields
 
-        public CustomElementLine()
-        {
-            pen.StartLineCap = PenLineCap.Round;
-            pen.EndLineCap = PenLineCap.Round;
-        }
-
-        #endregion
-
-        #region Private Properties
-
-        private Pen pen = new Pen();
-        private Brush previousStroke = null;
-        private double previousThickness = double.NaN;
-        private double penHalfThickness = double.NaN;
-
-        private Point A = new Point();
-        private Point B = new Point();
-        private Point C = new Point();
-        private Point D = new Point();
-        private double deltaX = double.NaN;
-        private double deltaY = double.NaN;
-        private bool isX = false;
-        private bool isY = false;
-
-        private double alpha = double.NaN;
-        private double theta = double.NaN;
-        private double zet = double.NaN;
-        private double headHeightY = double.NaN;
-        private double headHeightX = double.NaN;
-        private Point startCenter = new Point();
-        private Point endCenter = new Point();
+        private Pen pen;
+        private Brush previousStroke;
+        private double previousThickness;
+        private double penHalfThickness;
 
         #endregion
 
-        #region Overrides
+        #region Render
 
-        protected override void OnRender(System.Windows.Media.DrawingContext drawingContext)
+        private void UpdatePen()
         {
-            var dc = drawingContext;
-
-			if(this.pen == null)
-			{
-	            pen = new Pen(Stroke, Thickness)
-	            {
-	                StartLineCap = PenLineCap.Round,
-	                EndLineCap = PenLineCap.Round
-	            };
-
+            if (this.pen == null)
+            {
+                pen = new Pen(Stroke, Thickness)
+                {
+                    StartLineCap = PenLineCap.Round,
+                    EndLineCap = PenLineCap.Round
+                };
                 previousStroke = Stroke;
                 previousThickness = Thickness;
-
                 penHalfThickness = pen.Thickness / 2.0;
-			}
-			else 
-			{
-				if(Stroke != previousStroke || Thickness != previousThickness)
-				{
-					previousStroke = Stroke;
-					previousThickness = Thickness;
-					pen.Brush = Stroke;
-					pen.Thickness = Thickness;
-
+            }
+            else
+            {
+                if (Stroke != previousStroke || Thickness != previousThickness)
+                {
+                    previousStroke = Stroke;
+                    previousThickness = Thickness;
+                    pen.Brush = Stroke;
+                    pen.Thickness = Thickness;
                     penHalfThickness = pen.Thickness / 2.0;
-				}
-			}
+                }
+            }
+        }
 
-            A.X = X1;
-            A.Y = Y1;
+        protected override void OnRender(DrawingContext dc)
+        {
+            UpdatePen();
+            DrawLine(dc);
+        }
 
-            B.X = X2;
-            B.Y = Y2;
-
-            isX = (A.X == B.X);
-            isY = (A.Y == B.Y);
+        private void DrawLine(DrawingContext dc)
+        {
+            Point A = new Point(X1, Y1);
+            Point B = new Point(X2, Y2);
+            double deltaX;
+            double deltaY;
+            bool isX = (A.X == B.X);
+            bool isY = (A.Y == B.Y);
 
             if (EnableLineBreak == true && isX == false && isY == false)
             {
                 deltaX = B.X - A.X;
                 deltaY = B.Y - A.Y;
 
-                if ((StartAlignment == PinAlignment.Left || StartAlignment == PinAlignment.Right) &&
-                    (EndAlignment == PinAlignment.Left || EndAlignment == PinAlignment.Right))
+                if ((StartAlignment == PinAlignment.Left || StartAlignment == PinAlignment.Right)
+                    && (EndAlignment == PinAlignment.Left || EndAlignment == PinAlignment.Right))
                 {
-                    C.X = A.X + deltaX / 2.0;
-                    C.Y = A.Y;
+                    Point C = new Point(A.X + deltaX / 2.0, A.Y);
+                    Point D = new Point(A.X + deltaX / 2.0, B.Y);
 
-                    D.X = A.X + deltaX / 2.0;
-                    D.Y = B.Y;
-
-                    DrawGuidedLine(dc, A, C, true, false);
-                    DrawGuidedLine(dc, C, D, false, false);
-                    DrawGuidedLine(dc, D, B, false, true);
+                    DrawLine(dc, A, C, IsStartInverted, false);
+                    DrawLine(dc, C, D, false, false);
+                    DrawLine(dc, D, B, false, IsEndInverted);
                 }
-                else if ((StartAlignment == PinAlignment.Left || StartAlignment == PinAlignment.Right) &&
-                         (EndAlignment == PinAlignment.Top || EndAlignment == PinAlignment.Bottom))
+                else if ((StartAlignment == PinAlignment.Left || StartAlignment == PinAlignment.Right)
+                    && (EndAlignment == PinAlignment.Top || EndAlignment == PinAlignment.Bottom))
                 {
-                    C.X = A.X + deltaX;
-                    C.Y = A.Y;
+                    Point C = new Point(A.X + deltaX, A.Y);
 
-                    DrawGuidedLine(dc, A, C, true, false);
-                    DrawGuidedLine(dc, C, B, false, true);
+                    DrawLine(dc, A, C, IsStartInverted, false);
+                    DrawLine(dc, C, B, false, IsEndInverted);
                 }
-                else if ((StartAlignment == PinAlignment.Top || StartAlignment == PinAlignment.Bottom) &&
-                         (EndAlignment == PinAlignment.Top || EndAlignment == PinAlignment.Bottom))
+                else if ((StartAlignment == PinAlignment.Top || StartAlignment == PinAlignment.Bottom)
+                    && (EndAlignment == PinAlignment.Top || EndAlignment == PinAlignment.Bottom))
                 {
-                    C.X = A.X;
-                    C.Y = A.Y + deltaY / 2.0;
+                    Point C = new Point(A.X, A.Y + deltaY / 2.0);
+                    Point D = new Point(B.X, A.Y + deltaY / 2.0);
 
-                    D.X = B.X;
-                    D.Y = A.Y + deltaY / 2.0;
-
-                    DrawGuidedLine(dc, A, C, true, false);
-                    DrawGuidedLine(dc, C, D, false, false);
-                    DrawGuidedLine(dc, D, B, false, true);
+                    DrawLine(dc, A, C, IsStartInverted, false);
+                    DrawLine(dc, C, D, false, false);
+                    DrawLine(dc, D, B, false, IsEndInverted);
                 }
-                else if ((StartAlignment == PinAlignment.Top || StartAlignment == PinAlignment.Bottom) &&
-                         (EndAlignment == PinAlignment.Left || EndAlignment == PinAlignment.Right))
+                else if ((StartAlignment == PinAlignment.Top || StartAlignment == PinAlignment.Bottom)
+                    && (EndAlignment == PinAlignment.Left || EndAlignment == PinAlignment.Right))
                 {
-                    C.X = A.X;
-                    C.Y = A.Y + deltaY;
+                    Point C = new Point(A.X, A.Y + deltaY);
 
-                    DrawGuidedLine(dc, A, C, true, false);
-                    DrawGuidedLine(dc, C, B, false, true);
+                    DrawLine(dc, A, C, IsStartInverted, false);
+                    DrawLine(dc, C, B, false, IsEndInverted);
                 }
                 else
                 {
-                    DrawGuidedLine(dc, A, B, true, true);
+                    DrawLine(dc, A, B, IsStartInverted, IsEndInverted);
                 }
             }
             else
             {
-                DrawGuidedLine(dc, A, B, true, true);
+                DrawLine(dc, A, B, IsStartInverted, IsEndInverted);
             }
         }
 
-        #endregion
-
-        #region Methods
-
-        private void DrawGuidedLine(DrawingContext dc, Point start, Point end, bool enableStartInverted, bool enableEndInverted)
+        private void DrawLine(
+            DrawingContext dc, 
+            Point start, 
+            Point end, 
+            bool isStartInverted, 
+            bool isEndInverted)
         {
-            // calculate inverted start & end ellipse coordinates
-            if (enableStartInverted == true || enableEndInverted == true)
-            {
-                alpha = Math.Atan2(start.Y - end.Y, end.X - start.X);
-                theta = Math.PI - alpha;
-                zet = theta - Math.PI / 2;
+            double alpha = Math.Atan2(start.Y - end.Y, end.X - start.X);
+            double theta = Math.PI - alpha;
+            double zet = theta - Math.PI / 2;
+            double headHeightY = Math.Cos(zet) * (InvertedThickness + Thickness / 2);
+            double headHeightX = Math.Sin(zet) * (InvertedThickness + Thickness / 2);
 
-                headHeightY = Math.Cos(zet) * (InvertedThickness + Thickness / 2);
-                headHeightX = Math.Sin(zet) * (InvertedThickness + Thickness / 2);
-            }
-
-            // update & push line guidelines
-            GuidelineSet g = new GuidelineSet(new double[] { 0.0, 0.0 }, new double[] { 0.0, 0.0 });
-            g.GuidelinesX[0] = start.X + penHalfThickness;
-            g.GuidelinesX[1] = end.X + penHalfThickness;
-            g.GuidelinesY[0] = start.Y + penHalfThickness;
-            g.GuidelinesY[1] = end.Y + penHalfThickness;
+            GuidelineSet g = new GuidelineSet(
+                new double[] 
+                { 
+                    start.X + penHalfThickness, 
+                    end.X + penHalfThickness 
+                }, 
+                new double[] 
+                { 
+                    start.Y + penHalfThickness, 
+                    end.Y + penHalfThickness
+                });
             dc.PushGuidelineSet(g);
 
-            // draw inverted start ellipse
-            if (enableStartInverted == true && IsStartInverted == true)
+            if (isStartInverted == true)
             {
-                startCenter.X = start.X + headHeightX;
-                startCenter.Y = start.Y - headHeightY;
+                Point startCenter = new Point(start.X + headHeightX, start.Y - headHeightY);
 
                 start.X += 2 * headHeightX;
                 start.Y -= 2 * headHeightY;
@@ -343,11 +309,9 @@ namespace WireDiagram.CustomElements
                 dc.DrawEllipse(null, pen, startCenter, InvertedThickness, InvertedThickness);
             }
 
-            // draw inverted end ellipse
-            if (enableEndInverted == true && IsEndInverted == true)
+            if (isEndInverted == true)
             {
-                endCenter.X = end.X - headHeightX;
-                endCenter.Y = end.Y + headHeightY;
+                Point endCenter = new Point(end.X - headHeightX, end.Y + headHeightY);
 
                 end.X -= 2 * headHeightX;
                 end.Y += 2 * headHeightY; 
@@ -355,10 +319,7 @@ namespace WireDiagram.CustomElements
                 dc.DrawEllipse(null, pen, endCenter, InvertedThickness, InvertedThickness);
             }
 
-            // draw line
             dc.DrawLine(pen, start, end);
-
-            // pop line guidelines
             dc.Pop();
         }
 
